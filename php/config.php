9becf8117b49c5bf8ce45e9e04ddde46706861c9
Andrew Trieu <andrew.trieu@kempower.com>
2024-02-26 17:44:39 +0200

    Add Heroku config

--- php/config.php
+++ php/config.php
@@ -1,8 +1,12 @@
 <?php
-$hostname = "localhost:3200";
-$username = "root";
-$password = "";
-$dbname = "chatline";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$hostname = $cleardb_url["host"];
+$username = $cleardb_url["user"];
+$password = $cleardb_url["pass"];
+$dbname = substr($cleardb_url["path"], 1);
+
+$active_group = 'default';
+$query_builder = TRUE;
 
 $conn = mysqli_connect($hostname, $username, $password, $dbname);
 if (!$conn) {
